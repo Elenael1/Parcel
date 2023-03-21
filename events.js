ceb096e2fad93a1ea128ed9162caeb38abd975e0
Elenael1 <115032773+Elenael1@users.noreply.github.com>
2023-03-21 18:31:26 +0200

    parcel

---
+++ events.js
@@ -0,0 +1,48 @@
+// const number = document.querySelector("#number")
+// const increment = document.querySelector("#increment")
+// const decrement = document.querySelector("#decrement")
+// const remove = document.querySelector("#remove")
+
+
+// let value = 0;
+// value = Number(number.textContent);
+
+// increment.addEventListener("click", onClickIncrement);
+// decrement.addEventListener("click", onClickDecrement);
+// remove.addEventListener("click", onClickRemove);
+
+// function onClickIncrement(e) {
+//     number.innerHTML = value += 1;
+// }
+
+// function onClickDecrement(e) {
+//     number.innerHTML = value -= 1;
+//     if (value === -1) {
+//         decrement.removeEventListener("click", onClickDecrement)
+//     }
+// }
+
+// function onClickRemove(e) {
+//     decrement.removeEventListener("click", onClickDecrement)
+// }
+
+
+
+// document.addEventListener("keydown", onKeyDown);
+// document.addEventListener("keyup", onKeyUp);
+
+// function onKeyDown({ key, code }) {
+//     console.log(code)
+//     if (code === "Space") {
+//         console.log("I pressed space")
+//         document.body.style.backgroundColor = "black";
+//     }
+// }
+
+// function onKeyUp(event) {
+//     const code = event.code;
+//     if (event === "ControlLeft" && code === "AltLeft") {
+//         console.log(`I pressed ${code}`);
+//     }
+// }
+console.log('Heyy')
